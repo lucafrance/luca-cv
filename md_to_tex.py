467e921d48582eb473c1f4e11694be6ec13a9ef9
Luca Franceschini <luca.france@outlook.com>
2022-08-12 16:31:10 +0200

    Add CvSection and CvEntry

--- md_to_tex.py
+++ md_to_tex.py
@@ -13,14 +13,32 @@ def personal_info_from_md_line(txt):
     txt = txt.split("]")[0]
     return txt
 
+class CvSection():
+
+    def __init__(self, title):
+        self.title = title
+
+    def to_tex(self):
+        return "\section{{{}}}".format(self.title)
+
+class CvEntry():
+
+    def __init__(self, side_txt, title):
+        self.side_txt = side_txt
+        self.title = title
+
+    def to_tex(self):
+        return "\cventry{{{}}}{{{}}}{{}}{{}}{{}}{{}}".format(self.side_txt, self.title)
 
 class CurriculumVitae():
 
     def __init__(self):
         self.name = ("John", "Doe")
         self.title = None
+        self.content = None
 
     def from_markdown(self, markdown_src):
+        self.content = []
         lines = markdown_src.splitlines()
         i = 0
         while i < len(lines):
@@ -57,10 +75,29 @@ class CurriculumVitae():
                 self.github = personal_info_from_md_line(line)
                 i += 1
                 continue
+            
+            if line.startswith("## "):
+                self.content.append(CvSection(line.removeprefix("## ")))
+                i += 1
+                continue
+
+            if line.startswith("### "):
+                txt = line.removeprefix("### ")
+                if "*" not in txt:
+                    side_text = ""
+                    title = txt
+                else:
+                    txt = txt.split("*", 1)[1]
+                    txt = txt.split("*")
+                    side_text = txt[0].strip()
+                    title = txt[1].strip()
+                self.content.append(CvEntry(side_text, title))
+                i += 1
+                continue
 
             i += 1
 
-    def personal_data_md(self):
+    def personal_data_to_tex(self):
         tex_out = []
         tex_out.append("\\name {{{}}}{{{}}}".format(self.name[0], self.name[1]))
         if self.title is not None:
@@ -78,13 +115,15 @@ class CurriculumVitae():
         
         tex_out = "\n".join(tex_out)
         return tex_out
-        
+
+    def content_to_tex(self):
+        content_lines = [item.to_tex() for item in self.content]
+        return "\n".join(content_lines)
 
     def to_tex(self):
-        content = ""
         tex_template = open("cv_template.tex", "rt").read()
-        tex_template = tex_template.replace("$personal_data", self.personal_data_md())
-        tex_template = tex_template.replace("$content", content)
+        tex_template = tex_template.replace("$personal_data", self.personal_data_to_tex())
+        tex_template = tex_template.replace("$content", self.content_to_tex())
         return tex_template
 
 
