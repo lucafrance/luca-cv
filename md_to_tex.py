53ba297715cc678c1b054aa8672dcb2f07d3c885
Luca Franceschini <luca.france@outlook.com>
2022-08-12 11:42:44 +0200

    Add skeleton class and functions

--- md_to_tex.py
+++ md_to_tex.py
@@ -1,6 +1,17 @@
+import os.path
 import sys
+
+
+class CurriculumVitae():
+
+    def __init__(self, markdown_src):
+        pass
+
+    def to_tex(self):
+        return ""
 
 
 if __name__ == "__main__":
     src_filename = sys.argv[1]
-    print(src_filename)
+    cv = CurriculumVitae(open(src_filename, "rt").read())
+    open(os.path.splitext(src_filename)[0] + ".tex", "wt").write(cv.to_tex())
