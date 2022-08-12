5551c35c2e73499247a7bb58022822bb7b50fa2d
Luca Franceschini <luca.france@outlook.com>
2022-08-12 12:21:45 +0200

    Add tex template and working functions for name on CV

--- md_to_tex.py
+++ md_to_tex.py
@@ -1,17 +1,42 @@
 import os.path
 import sys
 
-
 class CurriculumVitae():
 
-    def __init__(self, markdown_src):
-        pass
+    def __init__(self):
+        self.name = ("John", "Doe")
+
+    def from_markdown(self, markdown_src):
+        lines = markdown_src.splitlines()
+        i = 0
+        while i < len(lines):
+            line = lines[i]
+            if line.startswith("# "):
+                line = line.removeprefix("# ")
+                self.name = line.split(" ", 1)
+                i += 1
+                continue
+        
+            i += 1
+
+    def personal_data_md(self):
+        tex_out = []
+        tex_out.append("\\name {{{}}}{{{}}}".format(self.name[0], self.name[1]))
+        
+        tex_out = "\n".join(tex_out)
+        return tex_out
+        
 
     def to_tex(self):
-        return ""
+        content = ""
+        tex_template = open("cv_template.tex", "rt").read()
+        tex_template = tex_template.replace("$personal_data", self.personal_data_md())
+        tex_template = tex_template.replace("$content", content)
+        return tex_template
 
 
 if __name__ == "__main__":
     src_filename = sys.argv[1]
-    cv = CurriculumVitae(open(src_filename, "rt").read())
+    cv = CurriculumVitae()
+    cv.from_markdown(open(src_filename, "rt").read())
     open(os.path.splitext(src_filename)[0] + ".tex", "wt").write(cv.to_tex())
