575dfd2e57598407386b7505a19e56bc2babe714
Luca Franceschini <luca.france@outlook.com>
2022-08-12 13:03:31 +0200

    Add personal information with links

--- md_to_tex.py
+++ md_to_tex.py
@@ -1,6 +1,19 @@
 import os.path
 import sys
 
+def personal_info_from_md_line(txt):
+    """Return personal info for lines with links
+    
+    e.g.
+    input: "`email`    [lucaf@lucaf.eu](mailto:lucaf@lucaf.eu)"
+    output: "lucaf@lucaf.eu"
+    """
+
+    txt = txt.split("[")[1]
+    txt = txt.split("]")[0]
+    return txt
+
+
 class CurriculumVitae():
 
     def __init__(self):
@@ -16,14 +29,30 @@ class CurriculumVitae():
                 line = line.removeprefix("# ")
                 self.name = line.split(" ", 1)
                 i += 1
-
                 # The first non-empty line after the name is the title
                 while lines[i] == "":
                     i += 1
                 self.title = lines[i]
                 i += 1
                 continue
-        
+            
+            if line.startswith("`email`"):
+                self.email = personal_info_from_md_line(line)
+                i += 1
+                continue
+            if line.startswith("`homepage`"):
+                self.homepage = personal_info_from_md_line(line)
+                i += 1
+                continue
+            if line.startswith("`linkedin`"):
+                self.linkedin = personal_info_from_md_line(line)
+                i += 1
+                continue
+            if line.startswith("`github`"):
+                self.github = personal_info_from_md_line(line)
+                i += 1
+                continue
+
             i += 1
 
     def personal_data_md(self):
@@ -31,6 +60,14 @@ class CurriculumVitae():
         tex_out.append("\\name {{{}}}{{{}}}".format(self.name[0], self.name[1]))
         if self.title is not None:
             tex_out.append("\\title{{{}}}".format(self.title))
+        if self.email is not None:
+             tex_out.append("\\email{{{}}}".format(self.email))
+        if self.homepage is not None:
+             tex_out.append("\\homepage{{{}}}".format(self.homepage))
+        if self.linkedin is not None:
+             tex_out.append("\\social[linkedin]{{{}}}".format(self.linkedin))
+        if self.github is not None:
+             tex_out.append("\\social[github]{{{}}}".format(self.github))
         
         tex_out = "\n".join(tex_out)
         return tex_out
